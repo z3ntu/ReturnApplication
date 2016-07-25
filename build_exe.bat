37e122d6170f4913e68a5d163a020c1c90b1d860
Luca Weiss <WEI16416@spengergasse.at>
2016-07-25 14:49:39 +0200

    update

---
+++ build_exe.bat
@@ -0,0 +1,1 @@
+pyinstaller -F -p "C:\users\luca\Local Settings\Application Data\Programs\Python\Python35-32\Lib\site-packages" main.py
--- main.py
+++ main.py
@@ -2,32 +2,34 @@ from time import sleep
 
 import requests
 import subprocess
-import config
 import logging
+import configparser
 
 
 def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
     last_status = "0"
     logging.warning("Started.")
-    # subprocess.call(config.batch_success_again)
+    subprocess.call(config['DEFAULT']['batch_success_again'])
     while True:
-        response = requests.get(config.url)
+        response = requests.get(config['DEFAULT']['url'])
         status = response.text.replace("\n", "")
         if status == "1":  # fail
             if last_status != "1":
                 print("Fail!")
                 logging.error("Failed!")
-                # subprocess.call(config.batch_fail)
+                subprocess.call(config['DEFAULT']['batch_fail'])
             else:
                 print("Fail but not calling script")
         elif last_status == "1":  # success again
             print("Works again.")
             logging.warning("Works again.")
-            # subprocess.call(config.batch_success_again)
+            subprocess.call(config['DEFAULT']['batch_success_again'])
         else:
             print("Works.")
         last_status = status
-        sleep(config.delay)
+        sleep(config['DEFAULT']['delay'])
 
 
 if __name__ == '__main__':
